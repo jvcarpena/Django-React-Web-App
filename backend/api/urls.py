eca561aac05f74753f2931b7e5a8dde4a5586e08
jvcarpena <jvcarpena2@gmail.com>
2025-03-04 09:23:50 +0800

    Added the note component

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,5 +5,5 @@ from .views import CreateUserView, NoteListCreateView, NoteDeleteView
 urlpatterns = [
     path('user/register/', CreateUserView.as_view(), name='register'),
     path('notes/', NoteListCreateView.as_view(), name='note-list'),
-    path('notes/delete/<int:pk>', NoteDeleteView.as_view(), name='delete-note')
+    path('notes/delete/<int:pk>/', NoteDeleteView.as_view(), name='delete-note')
 ]
